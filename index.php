77b2753d5949ffd8369c1da7dd25f523b9ff6fd0
HeppyH <nurheppyhutajulu@gmail.com>
2021-07-30 14:35:59 +0700

    added to more button

--- index.php
+++ index.php
@@ -16,6 +16,8 @@
         echo '<h1>My Name Is: '.$name.' </h1>';
     ?>
 <button type="button" class="btn btn-dark">CLICK ME!</button>
+<button type="button" class="btn btn-primary">CLICK ME!</button>
+<button type="button" class="btn btn-success">CLICK ME!</button>
 <?php
     require 'includes/footer.php'
 ?>
